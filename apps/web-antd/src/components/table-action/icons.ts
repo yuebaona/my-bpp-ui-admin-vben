3e55ab6e7b3f08c1fabbafa6c6e1cb8245cb4531
huangshanshan <551160839@qq.com>
2025-11-07 09:37:06 +0800

    style: 新增action_icon

--- apps/web-antd/src/components/table-action/icons.ts
+++ apps/web-antd/src/components/table-action/icons.ts
@@ -10,4 +10,7 @@ export const ACTION_ICON = {
   MORE: 'lucide:ellipsis-vertical',
   VIEW: 'lucide:eye',
   COPY: 'lucide:copy',
+  AUDIT: 'ant-design:audit-outlined',
+  UNDO: 'ant-design:undo-outlined',
+  LOG: 'material-symbols-light:docs-outline'
 };
