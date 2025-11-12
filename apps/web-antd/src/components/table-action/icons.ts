6a32d4040e1ea180e547c9d23592bfa5be02e4ba
huangshanshan <551160839@qq.com>
2025-11-12 15:01:03 +0800

    style: 新增图标

--- apps/web-antd/src/components/table-action/icons.ts
+++ apps/web-antd/src/components/table-action/icons.ts
@@ -12,5 +12,5 @@ export const ACTION_ICON = {
   COPY: 'lucide:copy',
   AUDIT: 'ant-design:audit-outlined',
   UNDO: 'ant-design:undo-outlined',
-  LOG: 'material-symbols-light:docs-outline'
+  LOG: 'material-symbols-light:docs-outline',
 };
