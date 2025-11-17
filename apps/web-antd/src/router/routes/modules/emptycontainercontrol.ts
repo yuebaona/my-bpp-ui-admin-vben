3875ce1d86876467fdf2a6669af2a319f0beaf07
lzt <1435828188@qq.com>
2025-11-17 11:52:17 +0800

    refactor: 空箱控箱管理页面移至bpp目录下

--- apps/web-antd/src/router/routes/modules/emptycontainercontrol.ts
+++ apps/web-antd/src/router/routes/modules/emptycontainercontrol.ts
@@ -3,7 +3,7 @@ import type { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/emptycontainercontrol',
-    component: () => import('#/views/emptycontainercontrol/index.vue'),
+    component: () => import('#/views/bpp/emptycontainercontrol/index.vue'),
     name: 'emptycontainercontrol',
     meta: {
       title: '空箱控箱',
