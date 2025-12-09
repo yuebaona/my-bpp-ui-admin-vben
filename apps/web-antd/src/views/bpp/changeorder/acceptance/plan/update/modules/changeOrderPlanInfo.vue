4b533cc0cf908d227d4f399dfe693655307447b1
huangshanshan <551160839@qq.com>
2025-12-09 15:23:57 +0800

    【功能新增】新增受理计划修改首页

---
+++ apps/web-antd/src/views/bpp/changeorder/acceptance/plan/update/modules/changeOrderPlanInfo.vue
@@ -0,0 +1,25 @@
+// 改单计划信息
+<script setup lang="ts">
+import { Card } from "ant-design-vue";
+
+import { useVbenForm } from '#/adapter/form';
+import { changeOrderPlanInfoFormSchema } from '#/views/bpp/changeorder/acceptance/plan/update/data';
+
+const [Form, formApi] = useVbenForm({
+  commonConfig: {
+    componentProps: { class: 'w-full' },
+    labelWidth: 150,
+  },
+  scrollToFirstError: true,
+  layout: 'horizontal',
+  showDefaultActions: false,
+  wrapperClass: 'grid-cols-1 md:grid-cols-2',
+  schema: changeOrderPlanInfoFormSchema(),
+});
+</script>
+
+<template>
+  <Card title="改单计划信息">
+    <Form />
+  </Card>
+</template>
