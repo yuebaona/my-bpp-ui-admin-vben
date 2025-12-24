0828cb033d3b043e13a9d9a5e2a1597433bca34a
suhong <136177947@qq.com>
2025-12-24 15:34:25 +0800

    【功能新增】日期选择组件修改

--- apps/web-antd/src/views/bpp/empty/container/control/data.ts
+++ apps/web-antd/src/views/bpp/empty/container/control/data.ts
@@ -443,7 +443,7 @@ export function PlanSearchFormSchema(): VbenFormSchema[] {
     {
       fieldName: 'createTime',
       label: '创建时间',
-      component: 'TimeRangePicker',
+      component: 'RangePicker',
       componentProps: {
         ...getRangePickerDefaultProps(),
         allowClear: true,
