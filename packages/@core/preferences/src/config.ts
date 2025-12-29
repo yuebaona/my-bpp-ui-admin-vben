e87e4f490063693b1e688e1ef5b917bc7c9599a7
luqixin <luqixin@bbwport.com>
2025-12-29 09:56:49 +0800

    【界面调整】调整菜单栏在顶部

--- packages/@core/preferences/src/config.ts
+++ packages/@core/preferences/src/config.ts
@@ -23,7 +23,7 @@ const defaultPreferences: Preferences = {
     enableRefreshToken: false,
     enableStickyPreferencesNavigationBar: true,
     isMobile: false,
-    layout: 'sidebar-nav',
+    layout: 'header-nav',
     locale: 'zh-CN',
     loginExpiredMode: 'page',
     name: 'porthg',
