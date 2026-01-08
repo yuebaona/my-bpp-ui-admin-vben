3bbf9b351d88c4dca4c428fdeb10a32e4d0c2ee2
huangshanshan <551160839@qq.com>
2026-01-08 18:37:57 +0800

    【功能新增】原计划与付费信息组件修改回显配置

--- apps/web-antd/src/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/modules/originalPlanPaymentInfo.vue
+++ apps/web-antd/src/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/modules/originalPlanPaymentInfo.vue
@@ -1,14 +1,28 @@
 // 原计划与付费信息组件
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
 import { useVbenModal, VbenButton } from '@vben/common-ui';
 
 import { Card, message } from 'ant-design-vue';
 
 import { useVbenForm } from '#/adapter/form';
+import {
+  selectByFormKeyNameType,
+  updateConfigForm,
+} from '#/api/bpp/flow/custom/config/form';
 import { $t } from '#/locales';
 import FormConfigModal from '#/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/components/FormConfigModal.vue';
 import { originalPlanPaymentInfoFormSchema } from '#/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/data';
-
+const customFormInfo = ref({
+  formKey: 'original_play_paymentInfo',
+  formName: '原计划与付费信息',
+  formType: 'originalPlanPaymentInfo',
+  formSchema: [],
+  id: '',
+});
+/** 表格展示用的行数据原始数据*/
+const localOriginalRows = ref<any[]>([]);
 const [Form, FormApi] = useVbenForm({
   commonConfig: {
     componentProps: { class: 'w-full' },
@@ -30,10 +44,37 @@ const openModal = () => {
   modalApi.setData(FormApi.getState()?.schema).open();
 };
 
-const handleSuccess = (resultData: []) => {
+const handleSuccess = async (resultData: []) => {
+  customFormInfo.value.formSchema = JSON.stringify(resultData);
   FormApi.setState({ schema: resultData });
+  await updateConfigForm(customFormInfo.value);
   message.success($t('ui.actionMessage.operationSuccess'));
 };
+onMounted(async () => {
+  localOriginalRows.value = FormApi.getState()?.schema;
+
+  const res = await selectByFormKeyNameType(customFormInfo.value);
+  if (res?.id) {
+    customFormInfo.value.id = res.id;
+  }
+  if (res?.formSchema) {
+    const schema = JSON.parse(res.formSchema);
+
+    schema.forEach((item: any) => {
+      if (item.fieldName) {
+        const originalItem = localOriginalRows.value.find(
+          (original: any) => original.fieldName === item.fieldName,
+        );
+
+        // 如果找到且有 rules，则替换（避免复杂校验规则不生效）
+        if (item?.rules) {
+          item.rules = originalItem.rules;
+        }
+      }
+    });
+    FormApi.setState({ schema });
+  }
+});
 </script>
 
 <template>
