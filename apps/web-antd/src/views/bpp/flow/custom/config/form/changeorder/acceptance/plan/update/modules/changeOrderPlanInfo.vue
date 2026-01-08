8531ac356f8befac50f78c66d14c29b6716c39e2
huangshanshan <551160839@qq.com>
2026-01-08 14:17:08 +0800

    【功能新增】改单计划信息组件修改回显配置

--- apps/web-antd/src/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/modules/changeOrderPlanInfo.vue
+++ apps/web-antd/src/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/modules/changeOrderPlanInfo.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts">
-import type { CustomConfigFormApi } from '#/api/bpp/flow/custom/config/form';
-
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 import { useVbenModal, VbenButton } from '@vben/common-ui';
 
 import { Card, message } from 'ant-design-vue';
 
 import { useVbenForm } from '#/adapter/form';
+import {
+  selectByFormKeyNameType,
+  updateConfigForm,
+} from '#/api/bpp/flow/custom/config/form';
 import { $t } from '#/locales';
 import FormConfigModal from '#/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/components/FormConfigModal.vue';
 import { changeOrderPlanInfoFormSchema } from '#/views/bpp/flow/custom/config/form/changeorder/acceptance/plan/update/data';
-
 /**
  * 自己唯一的表单配置信息,formKey应该对应受理计划类型的字典类型值，formType对应受理计划字典数据的字典键值，formName应该是本组件的name，此处只是示例
  */
 const customFormInfo = ref({
-  formKey: 'acceptance_plan_type',
+  formKey: 'change_order_planInfo',
   formName: '改单计划信息',
   formType: 'changeOrderPlanInfo',
   formSchema: [],
+  id: '',
 });
+/** 表格展示用的行数据原始数据*/
+const localOriginalRows = ref<any[]>([]);
 
 const [Form, FormApi] = useVbenForm({
   commonConfig: {
@@ -43,11 +47,37 @@ const openModal = () => {
   modalApi.setData(FormApi.getState()?.schema).open();
 };
 
-const handleSuccess = (resultData: []) => {
-
+const handleSuccess = async (resultData: []) => {
+  customFormInfo.value.formSchema = JSON.stringify(resultData);
   FormApi.setState({ schema: resultData });
+  await updateConfigForm(customFormInfo.value);
   message.success($t('ui.actionMessage.operationSuccess'));
 };
+onMounted(async () => {
+  localOriginalRows.value = FormApi.getState()?.schema;
+
+  const res = await selectByFormKeyNameType(customFormInfo.value);
+  if (res?.id) {
+    customFormInfo.value.id = res.id;
+  }
+  if (res?.formSchema) {
+    const schema = JSON.parse(res.formSchema);
+
+    schema.forEach((item: any) => {
+      if (item.fieldName) {
+        const originalItem = localOriginalRows.value.find(
+          (original: any) => original.fieldName === item.fieldName,
+        );
+
+        // 如果找到且有 rules，则替换（避免复杂校验规则不生效）
+        if (item?.rules) {
+          item.rules = originalItem.rules;
+        }
+      }
+    });
+    FormApi.setState({ schema });
+  }
+});
 </script>
 
 <template>
