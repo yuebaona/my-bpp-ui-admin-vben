2bfa00cdf5387deb06e130fadec3c7ac818e9da0
huangshanshan <551160839@qq.com>
2026-01-08 18:39:22 +0800

    【代码优化】字段调整

--- apps/web-antd/src/views/bpp/changeorder/acceptance/plan/update/data.ts
+++ apps/web-antd/src/views/bpp/changeorder/acceptance/plan/update/data.ts
@@ -4,16 +4,16 @@ import type { VxeTableGridOptions } from "@vben/plugins/vxe-table";
 export function changeOrderPlanInfoFormSchema(): VbenFormSchema[] {
   return [
     {
-      fieldName: 'applicantName',
+      fieldName: 'applyName',
       component: 'Input',
       label: '申请人',
       componentProps: {
-        placeholder: '请输入订单编号',
+        placeholder: '请输入申请编号',
       },
       rules: 'required',
     },
     {
-      fieldName: 'applicantPhone',
+      fieldName: 'applyPhone',
       component: 'Input',
       label: '申请人联系电话',
       componentProps: {
@@ -56,14 +56,14 @@ export function changeOrderPlanInfoFormSchema(): VbenFormSchema[] {
 export function originalPlanPaymentInfoFormSchema(): VbenFormSchema[] {
   return [
     {
-      fieldName: 'applicantName',
+      fieldName: 'applyName',
       component: 'Input',
       label: '申请人',
       componentProps: { placeholder: '请输入申请人' },
       rules: 'required',
     },
     {
-      fieldName: 'applicantPhone',
+      fieldName: 'applyPhone',
       component: 'Input',
       label: '申请人电话',
       componentProps: { placeholder: '请输入申请人电话' },
@@ -87,7 +87,7 @@ export function originalPlanPaymentInfoFormSchema(): VbenFormSchema[] {
       rules: 'required',
     },
     {
-      fieldName: 'applicantPlanCount',
+      fieldName: 'applyPlanCount',
       component: 'InputNumber',
       label: '计划数量',
       componentProps: { placeholder: '请输入计划数量' },
