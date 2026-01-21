fd2862f029ff51dc0810a095258b92619db08742
suhong <136177947@qq.com>
2026-01-21 17:01:52 +0800

    【功能完善】搜索栏创建时间添加子计划查询

--- apps/web-antd/src/views/bpp/empty/container/control/index.vue
+++ apps/web-antd/src/views/bpp/empty/container/control/index.vue
@@ -112,13 +112,15 @@ const [SubGrid, subGridApi] = useVbenVxeGrid({
       ajax: {
         query: async ({ page }, formValues) => {
           const mainFormValues = await mainGridApi.formApi.getValues();
-          const noFilterConditions = !hasSelectedMainPlan.value
-            && !mainFormValues.tradeType
-            && !mainFormValues.pickupPlanNo
-            && !mainFormValues.bayRangeList
-            && ownerCodeList.value.length === 0
-            && contIsoList.value.length === 0
-            && !dischargeVslSchedule.value;
+          const noFilterConditions =
+            !hasSelectedMainPlan.value &&
+            !mainFormValues.tradeType &&
+            !mainFormValues.pickupPlanNo &&
+            !mainFormValues.bayRangeList &&
+            !mainFormValues.createTime &&
+            ownerCodeList.value.length === 0 &&
+            contIsoList.value.length === 0 &&
+            !dischargeVslSchedule.value;
 
           if (noFilterConditions) {
             return { total: 0, list: [] };
@@ -135,13 +137,14 @@ const [SubGrid, subGridApi] = useVbenVxeGrid({
           if (mainFormValues.pickupPlanNo) {
             formValues.pickupPlanNo = mainFormValues.pickupPlanNo;
           }
+
           if (mainFormValues.bayRangeList) {
             formValues.bayRangeList = [
               {
                 yardBay: mainFormValues.bayRangeList,
                 yardRaw: '',
-              }
-            ]
+              },
+            ];
           }
           if (ownerCodeList.value) {
             formValues.ownerCodeList = ownerCodeList.value;
@@ -151,6 +154,13 @@ const [SubGrid, subGridApi] = useVbenVxeGrid({
           }
           if (dischargeVslSchedule.value) {
             formValues.dischargeVslSchedule = dischargeVslSchedule.value;
+          }
+          if (mainFormValues.createTime && mainFormValues.createTime.length > 0) {
+            formValues.createTime = mainFormValues.createTime
+              .map((time: string) => {
+                return time ? new Date(time).getTime() : null;
+              })
+              .filter(Boolean);
           }
           const result = await getSubPlanPage({
             pageNo: page.currentPage,
@@ -328,16 +338,14 @@ const [MainGrid, mainGridApi] = useVbenVxeGrid({
             } else {
               // 处理单个箱区的情况
               const upperCaseInput = bayRangeInput.toUpperCase();
-              if (upperCaseInput) {
-                queryParams.bayRangeList = [
-                  {
-                    yardBay: upperCaseInput,
-                    yardRaw: '',
-                  },
-                ];
-              } else {
-                queryParams.bayRangeList = [];
-              }
+              queryParams.bayRangeList = upperCaseInput
+                ? [
+                    {
+                      yardBay: upperCaseInput,
+                      yardRaw: '',
+                    },
+                  ]
+                : [];
             }
           } else {
             queryParams.bayRangeList = [];
