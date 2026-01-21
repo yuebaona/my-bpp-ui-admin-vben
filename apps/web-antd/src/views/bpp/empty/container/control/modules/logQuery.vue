54b9bcb329dee25b6720282fc5e07f88aa7406fa
lzt <1435828188@qq.com>
2026-01-21 17:37:05 +0800

    【功能完善】日志查询传参改为时间戳

--- apps/web-antd/src/views/bpp/empty/container/control/modules/logQuery.vue
+++ apps/web-antd/src/views/bpp/empty/container/control/modules/logQuery.vue
@@ -226,6 +226,13 @@ const [Grid, gridApi] = useVbenVxeGrid({
             pageSize: page.pageSize,
             ...formValues,
           };
+          // 将 createTime 从字符串转换为时间戳
+          if (data.createTime && data.createTime.length === 2) {
+            data.createTime = [
+              new Date(data.createTime[0]).getTime().toString(),
+              new Date(data.createTime[1]).getTime().toString()
+            ];
+          }
           const res = await getLogQueryPage(data);
           return {
             list: res.list,
