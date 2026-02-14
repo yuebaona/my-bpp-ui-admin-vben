96e0bec36e88601f09d1c55b595fcef624c6df22
suhong <136177947@qq.com>
2026-02-14 11:50:38 +0800

    【功能优化】添加条件要同时选择持箱人与iso接口才能打开闸口模拟选箱窗口

--- apps/web-antd/src/views/bpp/empty/container/control/index.vue
+++ apps/web-antd/src/views/bpp/empty/container/control/index.vue
@@ -441,6 +441,13 @@ function handleCreateMainPlan() {
 /** 闸口模拟选箱 */
 async function handleChooseContainer() {
   try {
+    if (
+      (contIsoList.value && contIsoList.value.length === 0) ||
+      (ownerCodeList.value && ownerCodeList.value.length === 0)
+    ) {
+      message.warning('请至少同时选择ISO和持箱人后再使用闸口模拟选箱');
+      return;
+    }
     if (ownerCodeList.value && ownerCodeList.value.length > 1) {
       message.warning('持箱人只能选择一个');
       return;
