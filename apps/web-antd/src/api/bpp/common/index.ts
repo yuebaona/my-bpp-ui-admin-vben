10b9a625926a70ca59bf71fa253af83edf6b8cb2
sgc <1041206119@qq.com>
2026-03-19 10:25:00 +0800

    【功能调整】获取客户基础信息接口url调整

--- apps/web-antd/src/api/bpp/common/index.ts
+++ apps/web-antd/src/api/bpp/common/index.ts
@@ -58,7 +58,7 @@ export namespace CommonApi {
 // 获取客户基础信息
 export const getCustomerList = (params: PageParam) => {
   return requestClient.get<PageResult<CommonApi.CustomerVO>>(
-    '/bpp/flow/common/get-customer-list',
+    '/bpp/base/customer/get-customer-list',
     { params },
   );
 };
